0fa9c06a91cf7bee0c0995290035e8ab36dab133
chenyl <chenyl@ppwriter.com>
2022-12-01 11:27:06 +0800

    init

---
+++ src/main/java/ink/dean/taskboot/TaskBootApplication.java
@@ -0,0 +1,13 @@
+package ink.dean.taskboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ink/dean/taskboot/TaskBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ink.dean.taskboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
