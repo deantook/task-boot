9f46e9a73ecf2bd8ea4cd33355faa27448351d77
chenyl <chenyl@ppwriter.com>
2022-12-01 14:10:35 +0800

    init nacos

--- src/main/java/ink/dean/taskboot/TaskBootApplication.java
+++ src/main/java/ink/dean/taskboot/TaskBootApplication.java
@@ -2,8 +2,10 @@ package ink.dean.taskboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TaskBootApplication {
 
     public static void main(String[] args) {
